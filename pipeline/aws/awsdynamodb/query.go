ea952e25cf3caf3bb6811e82f906d94161c4c55f
Nathan Simpson <nathan.simpson@spri.ng>
2025-01-07 23:56:01 +0000

    feat: add query function

--- pipeline/aws/awsdynamodb/query.go
+++ pipeline/aws/awsdynamodb/query.go
@@ -12,7 +12,7 @@ type Querier interface {
 	Query(ctx context.Context, input *dynamodb.QueryInput, opts ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error)
 }
 
-func sendQuery(ctx context.Context, q Querier, opts *Options) piper.Pipe {
+func sendQuery(q Querier, ctx context.Context, opts *Options) piper.Pipe {
 	return pipeline.Map(func(input *dynamodb.QueryInput) *dynamodb.QueryOutput {
 		output, err := q.Query(ctx, input, opts.DynamoDBOptions...)
 		if err != nil {
@@ -29,7 +29,7 @@ func FromQuery(q Querier, ctx context.Context, input *dynamodb.QueryInput, opts 
 		in      = make(chan any, 1)
 
 		p = pipeline.FromChannel(in).Thru(
-			sendQuery(ctx, q, options), // send dynamodb query
+			sendQuery(q, ctx, options), // send dynamodb query
 			dropIfNil(),                // if result is nil, do not pass it along
 		)
 	)
@@ -37,4 +37,20 @@ func FromQuery(q Querier, ctx context.Context, input *dynamodb.QueryInput, opts 
 	in <- input // pass in input
 	close(in)   // close the input channel
 	return p    // return the pipeline
+}
+
+type QueryMapFunction[In any] func(In) *dynamodb.QueryInput
+
+func mapToQueryInput[In any](mapfn QueryMapFunction[In]) piper.Pipe {
+	return pipeline.Map(func(input In) *dynamodb.QueryInput {
+		return mapfn(input)
+	})
+}
+
+func Query[In any](q Querier, ctx context.Context, mapfn QueryMapFunction[In], opts ...func(*Options)) piper.Pipe {
+	options := newClientOptions().apply(opts)
+	return piper.Join(
+		mapToQueryInput(mapfn),
+		sendQuery(q, ctx, options),
+	)
 }
